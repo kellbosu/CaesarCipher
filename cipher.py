4115a44c00a0e74e32307b2892eef0f22b199863
kellbosu <129923712+kellbosu@users.noreply.github.com>
2025-06-19 02:05:01 +0000

    Successfully was able to encrypt and decrypt input from user.

--- cipher.py
+++ cipher.py
@@ -7,20 +7,39 @@ def lifeInWeeks(CurrentAge):
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encrypt():
-    # take an input message
-    message = input("What message would you like to encrypt? ")
-    # take a desired shift number
-    shift = int(input("What is your shift number? "))
-    # move letters in message by shift number
+message_in = input("What message would you like to encrypt? ").lower()
+shift_num = int(input("What is your shift number? "))
+
+def encrypt(message, shift):
     encryptedMessage = ""
     for letter in message:
         if letter == " ":
-            encryptedMessage = encryptedMessage + " "
+            encryptedMessage += " "
         else:
-            encryptedMessage = encryptedMessage + alphabet[alphabet.index(letter) + int(shift)]
+            shiftedPosition = alphabet.index(letter) + int(shift)
+            # This is the best part of the program.  Using modulo cleverly to prevent index error.
+            shiftedPosition %= len(alphabet)
+            encryptedMessage += alphabet[shiftedPosition]
     print(encryptedMessage)
+    return encryptedMessage
 
-encrypt()
+
+def decrypt(message, shift):
+    decryptedMessage = ""
+    for letter in message:
+        if letter == " ":
+            decryptedMessage += " "
+        else:
+            shiftedPosition = alphabet.index(letter) - int(shift)
+            shiftedPosition %= len(alphabet)
+            decryptedMessage += alphabet[shiftedPosition]
+
+    print(decryptedMessage)
+            
+outputMessage = encrypt(message_in, shift_num)
+
+decrypt(outputMessage, shift_num)
+
+
 
 
